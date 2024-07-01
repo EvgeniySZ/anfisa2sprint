f8d97a43d6825e1fdba4ede356396c3447361bc4
EvgeniySZ <zhuk_es@mail.ru>
2024-07-01 23:36:11 +0300

    0107Home2

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,15 @@
+# homepage/views.py
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+ 
+    ice_cream_list = IceCream.objects.values('id', 'title')
+    
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    # Словарь контекста передаём в шаблон, рендерим HTML-страницу:
+    return render(request, template_name, context)
